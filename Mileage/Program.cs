c57767020608cb0f1de6d6cb261e01b9107a6263
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-04-06 02:30:06 -0500

    Sumbitted

--- Mileage/Program.cs
+++ Mileage/Program.cs
@@ -6,17 +6,27 @@ namespace Mileage
     {
         public static void Main()
         {
-            Console.Write("How many miles have you driven? ");
-
+            Console.Write("How many miles have you driven?: ");
             // Get input from the user and save it to a variable.
 
-            Console.Write("How many gallons of gas have you consumed? ");
+            string mileage = Console.ReadLine();
+            float miles = float.Parse(mileage);
 
+
+            Console.Write("How many gallons of gas have you consumed? ");
             // Get input from the user and save it to a second variable.
+
+            string gas = Console.ReadLine();
+            float gallons = float.Parse(gas);
 
             // Output - Your miles per gallon is (computed value)
 
+            float mpg = miles / gallons;
+
+            Console.WriteLine("Your miles per gallon is: " + mpg);
             Console.ReadLine();
+
+            
         }
     }
 }
